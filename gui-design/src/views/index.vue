f278ef0a729af2b721a38ba00a42f3f9c2e666a6
DreamZoom <1036199213@qq.com>
2019-01-23 23:50:04 +0800

    ss

--- gui-design/src/views/index.vue
+++ gui-design/src/views/index.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
 
-        <gui-template-editor v-model="html"></gui-template-editor>
-        <!-- <gui-h5-editor></gui-h5-editor> -->
+        <!-- <gui-template-editor v-model="html"></gui-template-editor> -->
+        <gui-h5-editor></gui-h5-editor>
     </div>
 </template>
 <script>
